0d07fe4fca78c694a703427544860e2ea72cf276
cboelter <christopher@boelter.eu>
2017-01-11 11:10:52 +0100

    added language files for notification center

---
+++ contao-module/languages/de/tl_nc_notification.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['leads_optin'] = 'Anfragen speichern';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['leads_optin_notification'][0] = 'OptIn';
